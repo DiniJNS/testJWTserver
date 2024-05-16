3248f8e3f22cb54147ed0d2215165f77cdf7eee6
Tsepo <3tnyarashe@gmail.com>
2024-05-16 11:02:18 +0200

    my commit

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,9 @@
 const express = require("express")
 const router = express.Router()
 const user = require('../controllers/auth_user_controller')
-const auth = require('../middleware/auth.middleware')
-
+// const auth = require('../middleware/auth.middleware')
 
 router.post('/register',user.signup)
-router.post('/signin', auth.verifyToken, user.login)
-
+router.post('/signin', /*auth.verifyToken,*/ user.login)
 
 module.exports = router
